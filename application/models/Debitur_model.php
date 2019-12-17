cdd4289acc5171f0e300ed12ee6d5048f010a9d0
kristt26 <kristt26@gmail.com>
2019-12-17 19:44:08 +0900

    New

--- application/models/Debitur_model.php
+++ application/models/Debitur_model.php
@@ -9,43 +9,44 @@ class Debitur_Model extends CI_Model
     }
     public function GetDebitur($Periode)
     {
+        // $result = $this->db->query("SELECT * FROM debitur");
+        // $debiturs = $result->result_object();
+        // $result = $this->db->get("kriteria");
+        // $Kriteria = $result->result_object();
+        // foreach ($debiturs as $key => $debitur) {
+        //     $result = $this->db->query("
+        //         SELECT
+        //             `persyaratan`.*,
+        //             `datapersyaratan`.`nilai`
+        //         FROM
+        //             `persyaratan`
+        //             LEFT JOIN `datapersyaratan` ON `persyaratan`.`idpersyaratan` =
+        //             `datapersyaratan`.`idpersyaratan`
+        //         WHERE
+        //             iddebitur = '$debitur->iddebitur'
+        //     ");
+        //     $debitur->persyaratan = $result->result_object();
+        //     $debitur->Kriteria = $Kriteria;
+        //     foreach ($debitur->Kriteria as $key1 => $value1) {
+        //         $result = $this->db->query("
+        //                 SELECT
+        //                     `subkriteria`.*,
+        //                     `datakriteria`.`nilai`
+        //                 FROM
+        //                     `subkriteria`
+        //                     LEFT JOIN `datakriteria` ON `subkriteria`.`idSubKriteria` =
+        //                     `datakriteria`.`idSubKriteria`
+        //                     LEFT JOIN `periode` ON `datakriteria`.`idperiode` = `periode`.`idperiode`
+        //                 WHERE
+        //                     `subkriteria`.idkriteria = '$value1->idkriteria' AND
+        //                     datakriteria.iddebitur = '$value->iddebitur' AND
+        //                     periode.idperiode = '$Periode->idperiode'
+        //         ");
+        //         $value1->subKriteria = $result->result_object();
+        //     }
+        // }
         $result = $this->db->query("SELECT * FROM debitur");
-        $debiturs = $result->result_object();
-        $result = $this->db->get("kriteria");
-        $Kriteria = $result->result_object();
-        foreach ($debiturs as $key => $debitur) {
-            $result = $this->db->query("
-                SELECT
-                    `persyaratan`.*,
-                    `datapersyaratan`.`nilai`
-                FROM
-                    `persyaratan`
-                    LEFT JOIN `datapersyaratan` ON `persyaratan`.`idpersyaratan` =
-                    `datapersyaratan`.`idpersyaratan`
-                WHERE
-                    iddebitur = '$debitur->iddebitur'
-            ");
-            $debitur->persyaratan = $result->result_object();
-            $debitur->Kriteria = $Kriteria;
-            foreach ($debitur->Kriteria as $key1 => $value1) {
-                $result = $this->db->query("
-                        SELECT
-                            `subkriteria`.*,
-                            `datakriteria`.`nilai`
-                        FROM
-                            `subkriteria`
-                            LEFT JOIN `datakriteria` ON `subkriteria`.`idSubKriteria` =
-                            `datakriteria`.`idSubKriteria`
-                            LEFT JOIN `periode` ON `datakriteria`.`idperiode` = `periode`.`idperiode`
-                        WHERE
-                            `subkriteria`.idkriteria = '$value1->idkriteria' AND
-                            datakriteria.iddebitur = '$value->iddebitur' AND
-                            periode.idperiode = '$Periode->idperiode'
-                ");
-                $value1->subKriteria = $result->result_object();
-            }
-        }
-        return $debiturs;
+        return $result->result_object();
     }
     public function UpdateDebitur($iddebitur, $data)
     {
